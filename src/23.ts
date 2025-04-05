709a2f638f08fa4459f2315ca47b48b7b49206f5
koskieschraub517 <koskieschraub517@gmail.com>
2025-04-05 06:22:09 +0000

    Add 23.ts

---
+++ src/23.ts
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy.optimize import fsolve
+
+def f(x):
+    return x**2 - 3*x + 1
+
+x0 = 1.5
+y0 = 4.5
+
+# Using fsolve to find the root of the function f with initial guess x0
+solution = fsolve(f, x0)
+
+print("The solution is:", solution[0])
