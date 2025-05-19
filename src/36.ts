b7c9efbbdf00c16cc6a65d940e72039bd42f6722
koskieschraub517 <koskieschraub517@gmail.com>
2025-05-19 03:28:39 +0000

    Add 36.ts

---
+++ src/36.ts
@@ -0,0 +1,10 @@
+import sympy as sp
+from sympy import symbols
+
+# Define symbols
+x, y = symbols('x y')
+
+# Solve the system of equations
+solution = sp.solve((x - 3) * (y + x), (x, y))
+
+print(solution)
