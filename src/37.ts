c562c17e9cfb81497338a33e30eaad416e380931
koskieschraub517 <koskieschraub517@gmail.com>
2025-05-20 10:01:24 +0000

    Add 37.ts

---
+++ src/37.ts
@@ -0,0 +1,9 @@
+import numpy as np
+from scipy.optimize import curve_fit
+
+def f(x, A, B):
+    return A * np.exp(-B * (x - 1))
+
+popt, _ = curve_fit(f, [0.5, 3.0], [2, 4])
+A, B = popt
+print(A, B)
